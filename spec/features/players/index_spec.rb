e6764ed8ceae2168ebd002752b1a3b922be3837e
Martin Chavez <martinchavez1203@gmail.com>
2024-01-31 13:45:39 -0700

    US 7, 8, 9

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Player, type: :feature do 
   describe 'players index' do
-    it 'shows all players' do
+    it 'displays all players' do
       team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
       team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
       player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
@@ -23,5 +23,29 @@ RSpec.describe Player, type: :feature do
       expect(page).to have_content(player3.hall_of_fame)
       expect(page).to have_content(player3.games_played)
     end
+
+    it 'has a child index link' do
+      team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
+      team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
+      player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
+      player2 = Player.create!(name: "Freddie Freeman", hall_of_fame: false, games_played: 100,team_id:team.id)
+      player3 = Player.create!(name: "Mookie Betts", hall_of_fame: false, games_played: 200, team_id:team.id)
+      
+      visit '/teams'
+
+      # save_and_open_page
+      expect(page).to have_link("players")
+
+      click_on("players")
+      
+      expect(current_path).to eq("/players")
+
+      expect(page).to have_content(player1.name)
+      expect(page).to have_content(player2.name)
+      expect(page).to have_content(player3.name)
+     
+    end
+
+
   end
 end
--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -16,13 +16,33 @@ RSpec.describe Team, type: :feature do
     it "shows teams sorted by most recent" do 
       team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true, created_at: Time.new(2021, 12, 03)  )
       team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false,created_at: Time.new(2022, 10, 30) )
-
+      # TALK ABOUT THIS ONE NEED CAPYBARA FOR THE EXPECTS 
       visit '/teams'
       # save_and_open_page
       expect(page).to have_content(team.name)
       expect(page).to have_content(team2.name)
 
+    end
 
+    it 'has a parent index link' do
+      team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
+      team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
+      player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
+      player2 = Player.create!(name: "Freddie Freeman", hall_of_fame: false, games_played: 100,team_id:team.id)
+      player3 = Player.create!(name: "Mookie Betts", hall_of_fame: false, games_played: 200, team_id:team.id)
+      
+      visit '/players'
+
+      expect(page).to have_link("teams")
+      
+      click_on("teams")
+      save_and_open_page
+      
+      expect(current_path).to eq("/teams")
+
+      expect(page).to have_content(team.name)
+      expect(page).to have_content(team2.name)
+     
     end
   end
 end
