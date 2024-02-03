d389a79645937c29b8c2c730c763efb1ce1cd694
Martin Chavez <martinchavez1203@gmail.com>
2024-02-02 21:10:43 -0700

    US 14 Complete

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,4 +10,10 @@ class PlayersController < ApplicationController
   def edit
     @player = Player.find(params[:id])
   end
+
+  def update 
+    player = Player.find(params[:id])
+    player.update(name: params[:name], hall_of_fame: params[:hall_of_fame], games_played: params[:games_played] )
+    redirect_to "/players/#{player.id}"
+  end
 end
--- spec/features/players/edit_spec.rb
+++ spec/features/players/edit_spec.rb
@@ -2,19 +2,45 @@ require "rails_helper"
 
 RSpec.describe "players update" do
   describe 'US 14' do
-    describe 'players update #PART1'
-    it 'goes to the player edit page ' do
-      team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
-      team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
-      player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
-      player2 = Player.create!(name: "Freddie Freeman", hall_of_fame: false, games_played: 100,team_id:team.id)
-      player3 = Player.create!(name: "Mookie Betts", hall_of_fame: false, games_played: 200, team_id:team.id)
+    describe 'players update #PART1' do 
+      it 'goes to the player edit page ' do
+        team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
+        team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
+        player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
+        player2 = Player.create!(name: "Freddie Freeman", hall_of_fame: false, games_played: 100,team_id:team.id)
+        player3 = Player.create!(name: "Mookie Betts", hall_of_fame: false, games_played: 200, team_id:team.id)
 
-      visit "/players/#{player1.id}"
+        visit "/players/#{player1.id}"
 
-      click_on("Update Player")
+        click_on("Update Player")
 
-      expect(current_path).to eq("/players/#{player1.id}/edit")
-    end
+        expect(current_path).to eq("/players/#{player1.id}/edit")
+      end
+    end 
+  end
+
+  describe 'US 14' do
+    describe 'Player update #PART2' do
+      it 'updates a player with new attributes' do
+        team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
+        team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
+        player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
+        player2 = Player.create!(name: "Freddie Freeman", hall_of_fame: false, games_played: 100,team_id:team.id)
+        player3 = Player.create!(name: "Mookie Betts", hall_of_fame: false, games_played: 200, team_id:team.id)
+
+        visit "/players/#{player1.id}/edit"
+
+        fill_in("name", with: "Todd Helton")
+        fill_in("hall_of_fame", with: false)
+        fill_in('games_played', with: 300)
+
+        click_on("Update #{player1.name}")
+
+        expect(current_path).to eq("/players/#{player1.id}")
+
+        expect(page).to have_content(false)
+        expect(page).to have_content(300)
+      end
+    end 
   end
 end
