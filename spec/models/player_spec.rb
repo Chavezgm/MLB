fd788e9a03009e33a851e95863bb9746e2429a16
Martin Chavez <martinchavez1203@gmail.com>
2024-02-02 21:47:48 -0700

    add true record method spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Player, type: :model do
   describe '#method_name' do
     it {should belong_to (:team)}
   end
+
+  describe 'true_record' do
+    it 'only shows players that have gone to the hall of fame' do
+      team = Team.create!(name: "Dodgers", year_founded:1884, world_series_appearance: true )
+      team2 = Team.create!(name: "Rockies", year_founded:1993, world_series_appearance: false )
+      player1 = Player.create!(name: "Todd Helton", hall_of_fame: true, games_played: 2247, team_id:team2.id)
+      player2 = Player.create!(name: "Freddie Freeman", hall_of_fame: false, games_played: 100,team_id:team.id)
+      player3 = Player.create!(name: "Mookie Betts", hall_of_fame: false, games_played: 200, team_id:team.id)
+      
+      expect(Player.true_records).to eq([player1])
+      expect(Player.true_records).to_not eq([player2,player3])
+    end
+  end
 end
